00f58575a26722855692ffd4868dc6f8d55ae975
johnb432 <58661205+johnb432@users.noreply.github.com>
2020-09-13 18:44:15 +0200

    Initial commit

---
+++ addons/fix_3cb_baf/CfgAcc.hpp
@@ -0,0 +1,49 @@
+class asdg_SlotInfo;
+
+class PointerSlot;
+class PointerSlot_rail: PointerSlot {
+    class compatibleItems {
+        uk3cb_baf_llm_flashlight_black = 1;
+        uk3cb_baf_llm_flashlight_tan = 1;
+        uk3cb_baf_llm_ir_black = 1;
+        uk3cb_baf_llm_ir_tan = 1;
+    };
+};
+
+class CowsSlot;
+class CowsSlot_Rail: CowsSlot {
+    class compatibleItems {
+        uk3cb_baf_kite = 1;
+        uk3cb_baf_maxikite = 1;
+        uk3cb_baf_specterlds = 1;
+        uk3cb_baf_suit = 1;
+        uk3cb_baf_susat = 1;
+        uk3cb_baf_ta31f = 1;
+        uk3cb_baf_ta31f_hornbill = 1;
+        uk3cb_baf_ta648 = 1;
+        uk3cb_baf_ta648_308 = 1;
+    };
+};
+
+class asdg_OpticRail: asdg_SlotInfo {
+    class compatibleItems {};
+};
+class asdg_OpticRail1913_3CB_FNFAL: asdg_OpticRail {
+    class compatibleItems {
+        uk3cb_baf_suit = 1;
+    };
+};
+class asdg_OpticRailL1A1: asdg_OpticRail {
+    class compatibleItems: compatibleItems {
+        uk3cb_baf_suit = 1;
+    };
+};
+
+class asdg_MuzzleSlot: asdg_SlotInfo {
+    class compatibleItems {};
+};
+class asdg_MuzzleSlot_9MM_SMG: asdg_MuzzleSlot {
+    class compatibleItems {
+        uk3cb_baf_silencer_l91a1 = 1;
+    };
+};
---
+++ addons/fix_3cb_baf/CfgMagazineWells.hpp
@@ -0,0 +1,174 @@
+class CfgMagazineWells {
+    class CBA_12g_8rnds {
+        WB_Magazines[] += {
+            "UK3CB_BAF_12G_Pellets",
+            "UK3CB_BAF_12G_Slugs",
+            "sfp_12Gauge_8rd_Pellets",
+            "sfp_12Gauge_8rd_Slug",
+            "ACE_8Rnd_12Gauge_Pellets_No00_Buck",
+            "ACE_8Rnd_12Gauge_Pellets_No0_Buck",
+            "ACE_8Rnd_12Gauge_Pellets_No1_Buck",
+            "ACE_8Rnd_12Gauge_Pellets_No2_Buck",
+            "ACE_8Rnd_12Gauge_Pellets_No3_Buck",
+            "ACE_8Rnd_12Gauge_Pellets_No4_Buck",
+            "ACE_8Rnd_12Gauge_Pellets_No4_Bird",
+            "ACE_8Rnd_12Gauge_Pellets_Slug_Buck"
+        };
+    };
+
+    class CBA_40mm_M203 {
+        WB_Magazines[] += {
+            "UK3CB_BAF_1Rnd_HE_Grenade_Shell",
+            "UK3CB_BAF_1Rnd_HEDP_Grenade_Shell",
+            "UK3CB_BAF_1Rnd_Blank_Grenade_Shell",
+            "UK3CB_BAF_UGL_FlareWhite_F",
+            "UK3CB_BAF_UGL_FlareRed_F",
+            "UK3CB_BAF_UGL_FlareGreen_F",
+            "UK3CB_BAF_UGL_FlareYellow_F",
+            "UK3CB_BAF_UGL_FlareCIR_F",
+            "UK3CB_BAF_1Rnd_Smoke_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokeRed_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokeGreen_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokeYellow_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokePurple_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokeBlue_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokeOrange_Grenade_shell"
+        };
+    };
+
+    class CBA_50BMG_M107 {
+        WB_Magazines[] += {
+            "UK3CB_BAF_127_10Rnd",
+            "UK3CB_BAF_127_10Rnd_AP"
+        };
+    };
+
+    class CBA_556x45_MINIMI {
+        WB_Magazines[] += {
+            "UK3CB_BAF_556_100Rnd_Blank",
+            "UK3CB_BAF_556_100Rnd",
+            "UK3CB_BAF_556_100Rnd_T",
+            "UK3CB_BAF_556_200Rnd_Blank",
+            "UK3CB_BAF_556_200Rnd",
+            "UK3CB_BAF_556_200Rnd_T"
+        };
+    };
+
+    class CBA_556x45_STANAG {
+        WB_Magazines[] += {
+            "UK3CB_BAF_556_30Rnd",
+            "UK3CB_BAF_556_30Rnd_Blank",
+            "UK3CB_BAF_556_30Rnd_T"
+        };
+    };
+
+    class CBA_762x51_FAL {
+        WB_Magazines[] += {
+            "UK3CB_BAF_762_20Rnd_Blank",
+            "UK3CB_BAF_762_20Rnd",
+            "UK3CB_BAF_762_20Rnd_T"
+        };
+    };
+
+    class CBA_762x51_LINKS {
+        WB_Magazines[] += {
+            "UK3CB_BAF_762_100Rnd_Blank",
+            "UK3CB_BAF_762_100Rnd",
+            "UK3CB_BAF_762_100Rnd_T",
+            "UK3CB_BAF_762_200Rnd_Blank",
+            "UK3CB_BAF_762_200Rnd",
+            "UK3CB_BAF_762_200Rnd_T"
+        };
+    };
+
+    class CBA_762x51_M14 {
+        WB_Magazines[] += {
+            "UK3CB_BAF_762_L42A1_20Rnd_Blank",
+            "UK3CB_BAF_762_L42A1_20Rnd",
+            "UK3CB_BAF_762_L42A1_20Rnd_T"
+        };
+    };
+
+    class CBA_762x51_SR25 {
+        WB_Magazines[] += {
+            "UK3CB_BAF_762_L42A1_20Rnd_Blank",
+            "UK3CB_BAF_762_L42A1_20Rnd",
+            "UK3CB_BAF_762_L42A1_20Rnd_T"
+        };
+    };
+
+    class CBA_9x19_MP5 {
+        WB_Magazines[] += {
+            "UK3CB_BAF_9_30Rnd"
+        };
+    };
+
+    class CBA_9x19_P226 {
+        WB_Magazines[] += {
+            "UK3CB_BAF_9_15Rnd"
+        };
+    };
+
+    class CBA_9x19_P228 {
+        WB_Magazines[] += {
+            "UK3CB_BAF_9_13Rnd",
+            "UK3CB_BAF_9_15Rnd"
+        };
+    };
+
+    class CBA_9x19_Glock_Full {
+        WB_Magazines[] += {
+            "UK3CB_BAF_9_17Rnd"
+        };
+    };
+
+    class M240_762x51 {
+        WB_Magazines[] += {
+            "UK3CB_BAF_762_100Rnd_Blank",
+            "UK3CB_BAF_762_100Rnd",
+            "UK3CB_BAF_762_100Rnd_T",
+            "UK3CB_BAF_762_200Rnd_Blank",
+            "UK3CB_BAF_762_200Rnd",
+            "UK3CB_BAF_762_200Rnd_T"
+        };
+    };
+
+    class M249_556x45 {
+        WB_Magazines[] += {
+            "UK3CB_BAF_556_100Rnd_Blank",
+            "UK3CB_BAF_556_100Rnd",
+            "UK3CB_BAF_556_100Rnd_T",
+            "UK3CB_BAF_556_200Rnd_Blank",
+            "UK3CB_BAF_556_200Rnd",
+            "UK3CB_BAF_556_200Rnd_T"
+        };
+    };
+
+    class STANAG_556x45 {
+        WB_Magazines[] += {
+            "UK3CB_BAF_556_30Rnd",
+            "UK3CB_BAF_556_30Rnd_Blank",
+            "UK3CB_BAF_556_30Rnd_T"
+        };
+    };
+
+    class UGL_40x36 {
+        WB_Magazines[] += {
+            "UK3CB_BAF_1Rnd_HE_Grenade_Shell",
+            "UK3CB_BAF_1Rnd_HEDP_Grenade_Shell",
+            "UK3CB_BAF_1Rnd_Blank_Grenade_Shell",
+            "UK3CB_BAF_UGL_FlareWhite_F",
+            "UK3CB_BAF_UGL_FlareRed_F",
+            "UK3CB_BAF_UGL_FlareGreen_F",
+            "UK3CB_BAF_UGL_FlareYellow_F",
+            "UK3CB_BAF_UGL_FlareCIR_F",
+            "UK3CB_BAF_1Rnd_Smoke_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokeRed_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokeGreen_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokeYellow_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokePurple_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokeBlue_Grenade_shell",
+            "UK3CB_BAF_1Rnd_SmokeOrange_Grenade_shell"
+        };
+    };
+};
